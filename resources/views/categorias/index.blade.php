2e82f454c03c1570102c4210f8e252537d528ead
unknown <Sena@MEDAPRCSGFD814.FormacionSena.RED>
2022-06-06 18:35:37 -0500

    06-06-2022

---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,26 @@
+
+@extends('principal')
+
+@section('contenido')
+
+
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th></th>
+        </tr>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </thead>
+</table>
+
+@endsection
